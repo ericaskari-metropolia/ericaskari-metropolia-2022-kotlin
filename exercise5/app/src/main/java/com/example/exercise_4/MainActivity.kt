8eac2e4ca80e7ae59ef7d10f4f29ff1b45e54d03
Eric Askari <ericaskari@gmail.com>
2022-09-13 14:38:32 +0300

    e5

--- exercise5/app/src/main/java/com/example/exercise_4/MainActivity.kt
+++ exercise5/app/src/main/java/com/example/exercise_4/MainActivity.kt
@@ -2,18 +2,19 @@ package com.example.exercise_5
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.exercise_5.adapters.ParliamentMemberListItemRecycleViewAdapter
 import com.example.exercise_5.databinding.ActivityMainBinding
 import com.example.exercise_5.datasource.ParliamentMembersData
-import com.example.exercise_5.fragments.ParliamentMemberCardFragment
 import com.example.exercise_5.interfaces.OnParliamentClickListener
 import com.example.exercise_5.models.Parliament
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main), OnParliamentClickListener {
+    private val parliament = Parliament(ParliamentMembersData.members)
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +23,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), OnParliamentClic
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val p = Parliament(ParliamentMembersData.members)
-
-        val adapter = ParliamentMemberListItemRecycleViewAdapter(this, p.members)
+        val adapter = ParliamentMemberListItemRecycleViewAdapter(this, parliament.members)
 
         //  Set recycleView adapter
         this.binding.recycleView.adapter = adapter
@@ -34,5 +33,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), OnParliamentClic
     }
 
     override fun onItemClick(v: View?, index: Number) {
+        val item = parliament.members[index.toInt()]
+        Toast.makeText(applicationContext,"Clicked on ${item.fullName()}",Toast.LENGTH_SHORT).show()
     }
 }
