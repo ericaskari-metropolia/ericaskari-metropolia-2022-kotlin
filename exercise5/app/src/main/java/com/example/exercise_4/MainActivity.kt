6d82a1c131b4e0abc32aae277aecd9b39596cad5
Eric Askari <ericaskari@gmail.com>
2022-09-13 14:17:44 +0300

    e5

--- exercise5/app/src/main/java/com/example/exercise_4/MainActivity.kt
+++ exercise5/app/src/main/java/com/example/exercise_4/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.exercise_5
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.exercise_5.adapters.ParliamentMemberListItemRecycleViewAdapter
 import com.example.exercise_5.databinding.ActivityMainBinding
 import com.example.exercise_5.datasource.ParliamentMembersData
 import com.example.exercise_5.fragments.ParliamentMemberCardFragment
+import com.example.exercise_5.interfaces.OnParliamentClickListener
 import com.example.exercise_5.models.Parliament
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+class MainActivity : AppCompatActivity(R.layout.activity_main), OnParliamentClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,18 +24,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         val p = Parliament(ParliamentMembersData.members)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                add(R.id.fragment_container_view, ParliamentMemberCardFragment(p.members.random()))
-                setReorderingAllowed(true)
-            }
-        }
+        val adapter = ParliamentMemberListItemRecycleViewAdapter(this, p.members)
 
-        binding.viewNext.setOnClickListener {
-            supportFragmentManager.commit {
-                replace(R.id.fragment_container_view, ParliamentMemberCardFragment(p.members.random()))
-                setReorderingAllowed(true)
-            }
-        }
+        //  Set recycleView adapter
+        this.binding.recycleView.adapter = adapter
+
+        //  Set grid layout with two columns
+        this.binding.recycleView.layoutManager = GridLayoutManager(null, 1)
+    }
+
+    override fun onItemClick(v: View?, index: Number) {
     }
 }
--- exercise5/app/src/main/java/com/example/exercise_4/adapters/ParliamentListItemRecycleViewAdapter.kt
+++ exercise5/app/src/main/java/com/example/exercise_4/adapters/ParliamentListItemRecycleViewAdapter.kt
@@ -1,38 +1,36 @@
 package com.example.exercise_5.adapters
 
-// Commented out and will be used for future feature
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.exercise_5.databinding.ParliamentMemberCardItemBinding
+import com.example.exercise_5.interfaces.OnParliamentClickListener
+import com.example.exercise_5.models.ParliamentMember
+import com.example.exercise_5.viewholders.ParliamentListItemRecycleViewHolder
 
-//import android.view.LayoutInflater
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.exercise_5.databinding.ParliamentMemberCardItemBinding
-//import com.example.exercise_5.interfaces.OnParliamentClickListener
-//import com.example.exercise_5.models.ParliamentMember
-//import com.example.exercise_5.viewholders.ParliamentListItemRecycleViewHolder
+class ParliamentMemberListItemRecycleViewAdapter(
+    private val onParliamentClickListener: OnParliamentClickListener,
+    private val parliamentMemberList: List<ParliamentMember>
+) : RecyclerView.Adapter<ParliamentListItemRecycleViewHolder>() {
 
-//class ParliamentMemberListItemRecycleViewAdapter(
-//    private val onParliamentClickListener: OnParliamentClickListener,
-//    private val parliamentMemberList: List<ParliamentMember>
-//) : RecyclerView.Adapter<ParliamentListItemRecycleViewHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParliamentListItemRecycleViewHolder {
-//        val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
-//
-//        val binding: ParliamentMemberCardItemBinding =
-//            ParliamentMemberCardItemBinding.inflate(layoutInflater, parent, false)
-//
-//        return ParliamentListItemRecycleViewHolder(binding)
-//    }
-//
-//    /**
-//     * Inject data into list items.
-//     */
-//    override fun onBindViewHolder(holder: ParliamentListItemRecycleViewHolder, position: Int) {
-//        holder.setViewHolderData(parliamentMemberList[position], this.onParliamentClickListener, position);
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return this.parliamentMemberList.size
-//    }
-//
-//}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParliamentListItemRecycleViewHolder {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
+
+        val binding: ParliamentMemberCardItemBinding =
+            ParliamentMemberCardItemBinding.inflate(layoutInflater, parent, false)
+
+        return ParliamentListItemRecycleViewHolder(binding)
+    }
+
+    /**
+     * Inject data into list items.
+     */
+    override fun onBindViewHolder(holder: ParliamentListItemRecycleViewHolder, position: Int) {
+        holder.setViewHolderData(parliamentMemberList[position], this.onParliamentClickListener, position);
+    }
+
+    override fun getItemCount(): Int {
+        return this.parliamentMemberList.size
+    }
+
+}
